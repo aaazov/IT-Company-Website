8931e6ebb9bd8dea73d0f1b01cd0964e20bd28be
aaazov <102871283+aaazov@users.noreply.github.com>
2025-12-30 12:52:52 +0800

    modify peter intro and address

---
+++ js/config.js
@@ -0,0 +1,4 @@
+const CONFIG = {
+  DEV_URL:'https://script.google.com/macros/s/AKfycbw3ZK3MQ1Cr2GKvrPSeS3TPCvcmTdE3aZZVHYlfuw_u/dev',
+  EXEC_URL:'https://script.google.com/macros/s/AKfycbwEGA_OeVhw3qfRTaTfGtaLjPYNBIhLJWW6vWY69Q-w767wpABUib5QZQzgNaTi9xef1g/exec',
+};
